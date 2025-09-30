a31e305efeeefc8eda5d110eaf94432f7f19d6be
psicofil <psicofil@gmail.com>
2025-09-30 09:48:23 -0300

    Update Geom Menu for Plugins

--- Plugins/geom_plugins.py
+++ Plugins/geom_plugins.py
@@ -4,33 +4,35 @@ import salome_pluginsmanager
 
 def filter_group(context):
   import geom_filter_group
+  from importlib import reload
   reload(geom_filter_group)
 
 def contact(context):
   import geom_3d_contact
+  from importlib import reload
   reload(geom_3d_contact)
 
 def pass_group(context):
   import geom_pass_group
+  from importlib import reload
   reload(geom_pass_group)
 
-def internal_group(context):
-  import geom_internal_contour
-  reload(geom_internal_contour)
-    
+def line_from_circle(context):
+  import geom_line_from_circle
+  from importlib import reload
+  reload(geom_line_from_circle)
 
 
-salome_pluginsmanager.AddFunction('Psicofil Plugins/Create Group from Filter',
-                                  'Create a similar group according criteria',
+salome_pluginsmanager.AddFunction('Scope Plugins/Create Groups from Filter.',
+                                  'Create Groups Based on Multiple Criteria.',
                                   filter_group)
-salome_pluginsmanager.AddFunction('Psicofil Plugins/Detect 3D contacts',
-                                  'Automatically detect 3D contacts of two o more parts.',
+salome_pluginsmanager.AddFunction('Scope Plugins/Detect 3D contacts',
+                                  'Automatically Detect 3D contacts of two o more parts.',
                                   contact)
-salome_pluginsmanager.AddFunction('Psicofil Plugins/Pass Geometrical Groups',
-                                  'Pass similar groups from a part to another',
+salome_pluginsmanager.AddFunction('Scope Plugins/Pass Geometrical Groups',
+                                  'Transfer similar groups from one part to another.',
                                   pass_group)
-salome_pluginsmanager.AddFunction('Psicofil Plugins/Internal Contour',
-                                  'Auto-selects the internal contour of a part. You have 2 options, select only the internal surface and set it as a group (for FEM), or create a new part with the control volume (for CFD).',
-                                  internal_group)
-
+salome_pluginsmanager.AddFunction('Scope Plugins/Lines from circles',
+                                  'Create Edges/Lines between two groups of circles.',
+                                  line_from_circle)
 
